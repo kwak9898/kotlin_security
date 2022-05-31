4ea78ea95485607121a44bdde79f32056a0ad1f4
kwaktaemin <kwak9898@github.com>
2022-05-31 18:20:23 +0900

    Account Add

--- src/main/kotlin/com/security/study/kotlin_security/account/Account.kt
+++ src/main/kotlin/com/security/study/kotlin_security/account/Account.kt
@@ -1,11 +1,32 @@
 package com.security.study.kotlin_security.account
 
+import org.hibernate.annotations.CreationTimestamp
+import org.springframework.security.core.authority.SimpleGrantedAuthority
 import java.time.LocalDateTime
+import javax.persistence.*
 
+@Entity
 data class Account(
-    var id: Long,
+    @Id @GeneratedValue
+    var id: Long? = null,
     var email: String,
     var password: String,
+
+    @Enumerated(EnumType.STRING)
+    @ElementCollection(fetch = FetchType.EAGER)
     var roles: Set<AccountRole>,
-    var createDt: LocalDateTime
-)
+
+    @CreationTimestamp
+    var createDt: LocalDateTime = LocalDateTime.now()
+) {
+    fun getAuthorities(): User {
+        return User(
+            this.email, this.password,
+            this.roles.stream().map { role -> SimpleGrantedAuthority("ROLE_$role") }.collect(Collectors.toSet())
+        )
+    }
+}
+
+enum class AccountRole {
+    ADMIN, USER
+}
