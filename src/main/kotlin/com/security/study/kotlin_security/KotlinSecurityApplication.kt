078c0a987579ddea6579f3a4bc4e56b0fb9f2466
kwaktaemin <kwak9898@github.com>
2022-05-31 16:27:44 +0900

    Kotlin Spring Security

---
+++ src/main/kotlin/com/security/study/kotlin_security/KotlinSecurityApplication.kt
@@ -0,0 +1,11 @@
+package com.security.study.kotlin_security
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSecurityApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinSecurityApplication>(*args)
+}
